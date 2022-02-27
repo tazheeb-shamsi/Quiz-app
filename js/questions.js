c7e4bb6e1557de6e4757afcadb79e3725c91dd88
Geeklord-Tazheeb <thshamsi@hotmail.com>
2022-02-27 16:20:03 +0530

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+      numb: 6,
+      question: "What does AJAX stand for",
+      answer: "Asynchronous JavaScript And XML",
+      options: [
+        "AutoComplete JavaScript And XML",
+        "Asynchronous JavaScript And HTML",
+        "Automatic JavaScript And XML",
+        "Asynchronous JavaScript And XML"
+      ]
+    },
+      {
+      numb: 7,
+      question: "What does XML stand for",
+      answer: "extensible markup language",
+      options: [
+        "extensible moderate language",
+        "extensive markup language",
+        "extensible markup language",
+        "extensible maping language"
+      ]
+    },
+      {
+      numb: 8,
+      question: "What does GIF stand for",
+      answer: "Graphics Interchange Format",
+      options: [
+        "Graphics Interchange Format",
+        "Graphics Interchange Frame",
+        "Graphics Internal Form",
+        "Graph Interchange Format"
+      ]
+    },
+      {
+      numb: 9,
+      question: "What does HTTP stand for",
+      answer: "Hypertext Transfer Protocol",
+      options: [
+        "Hypertext Transfering Protocol",
+        "Hypertext Transfer Prots",
+        "Hypertext Transfer Protocol",
+        "Hypertext Transfer Portals"
+      ]
+    },
+      {
+      numb: 10,
+      question: "What does API stand for",
+      answer: "JavaScript XML",
+      options: [
+        "JavaScript Query Language",
+        "JavaScript XML",
+        "JavaScript extensive language",
+        "JavaScript Markup Language"
+      ]
+    },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
